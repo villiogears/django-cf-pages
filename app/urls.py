02d4b4428fec75b25d154ed7fc4c342d711b1cc3
Your Name <you@example.com>
2025-07-02 19:44:21 +0900

    aaaaa

---
+++ app/urls.py
@@ -0,0 +1,39 @@
+"""app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+# Replaces the standard django.urls.path, identical syntax
+from django_distill import distill_path
+
+# Import some views from your Django app
+from app.views import IndexView, PageView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # The index URL on /, render this as 'index.html'
+    distill_path('',
+                IndexView.as_view(),
+                name='index',
+                # / is not a valid file name! override it to index.html
+                distill_file='index.html'),
+
+    # A single static page, render this as 'page.html'
+    distill_path('page.html',
+                PageView.as_view(),
+                name='page'),
+]
